773d7f3781beede9139898481a15ad6d7196160e
RyCornel <ry.cornel@students.makeschool.com>
2021-01-09 21:26:17 -0800

    Initial Commit

---
+++ Yelp-Killer-2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yelp-Killer-2021
+//
+//  Created by Ryan Cornel on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
