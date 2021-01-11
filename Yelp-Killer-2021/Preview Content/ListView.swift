d8be2e427bd99ef88b2850caf435921fec9b28a9
RyCornel <ry.cornel@students.makeschool.com>
2021-01-11 13:15:50 -0800

    Commit

--- Yelp-Killer-2021/Preview Content/ListView.swift
+++ Yelp-Killer-2021/Preview Content/ListView.swift
@@ -8,42 +8,56 @@
 import SwiftUI
 
 struct ListView: View {
+    @State var brandNewDish: String = ""
+    
+    var textInputField: some View {
+        HStack {
+            TextField("Enter A New Dish Here", text: self.$brandNewDish)
+            TextField("Enter The Eatery", text: self.$brandNewDish)
+        }
+    }
     var topDishes: [Dish]
+    
+    
     var body: some View {
         NavigationView{
             List(newDishes){
             Dish in DishView(eachDish: Dish)
         }.navigationBarTitle(Text("Top 5 Dishes Near You"))
         }
+        textInputField
     }
-}
+    }
 
-struct DishView: View {
-    var eachDish: Dish
-    var body: some View {
+    struct DishView: View {
+        var eachDish: Dish
+        var body: some View {
                 HStack() {
                     Image("waves")
                         .resizable()
                         .frame(width: 75.0, height: 75.0)
                     Text(eachDish.dish)
                         .padding()
-                    
+                    Text(eachDish.spot)
+                        .padding()
                 }
+            
             }
             
         }
 
-var newDishes = [
-    Dish(id: 1, dish: "Burrito", spot:""),
-    Dish(id: 2, dish: "Sushi", spot:""),
-    Dish(id: 3, dish: "Burger", spot:""),
-    Dish(id: 4, dish: "Pizza", spot:""),
-    Dish(id: 5, dish: "Chowmein", spot:"")
-]
+    var newDishes = [
+        Dish(id: 1, dish: "Burrito", spot:"Los Altos Taqueria"),
+        Dish(id: 2, dish: "Sushi", spot:"Beyond Sushi"),
+        Dish(id: 3, dish: "Burger", spot:"Veggie Grill"),
+        Dish(id: 4, dish: "Pizza", spot:"Two Boots"),
+        Dish(id: 5, dish: "Chowmein", spot:"Veggie Garden")
+    ]
 
 
-struct ListView_Previews: PreviewProvider {
+    struct ListView_Previews: PreviewProvider {
     static var previews: some View {
         ListView(topDishes: newDishes)
     }
-}
+    }
+
