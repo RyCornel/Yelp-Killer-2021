b4121097ca0e7248c5802c14d6660daebfdb0aae
RyCornel <ry.cornel@students.makeschool.com>
2021-01-11 19:12:04 -0800

    Counter added.

--- Yelp-Killer-2021/ContentView.swift
+++ Yelp-Killer-2021/ContentView.swift
@@ -7,19 +7,60 @@
 
 import SwiftUI
 import Combine
+import MapKit
+import CoreLocation
 
 struct ContentView: View {
     @ObservedObject var dishStore = DishStore()
     @State var newDish : String = ""
     @State var newSpot : String = ""
+    @State var posCount : Int = 0
+    @State var negCount : Int = 0
     
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .leading) {
                 List(self.dishStore.dishes) { aDish in
                     Text(aDish.dish)
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .scaledToFill()
                     Text(aDish.spot)
-                } .navigationTitle("Top Dishes Near You")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .scaledToFill()
+                    Text("Likes")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .scaledToFill()
+                    VStack {
+                        Button(action:
+                                {self.posCount += 1}, label: {
+                                    Text("Vote Up \(posCount)")
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color.black)
+                                        .multilineTextAlignment(.leading)
+                                        .scaledToFit()
+                                })
+                        /*Button(action:
+                                {self.negCount -= 1}, label: {
+                                    Text("Vote Down")
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color.black)
+                                        .multilineTextAlignment(.leading)
+                                        .scaledToFill()
+                                }) */
+                    }
+                    }
+ .navigationTitle("Top Dishes Near You")
                 searchBar
             }
         }
@@ -40,6 +81,9 @@ struct ContentView: View {
     
     func addNewDish() {
         dishStore.dishes.append(Dish(id: String(dishStore.dishes.count + 1), dish: newDish, spot: newSpot))
+        
+        self.newDish = ""
+        self.newSpot = ""
         
     }
 }
